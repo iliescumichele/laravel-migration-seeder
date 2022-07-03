160a60e54141d3242c3c401f3f9403ed9329ef58
iliescumichele <iliescumichele@gmail.com>
2022-07-03 21:53:53 +0200

    Creato controlle, creata tabella e fatto la migrazione

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,6 +7,8 @@
 
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css">
+
     <title>Home</title>
 </head>
 <body>
